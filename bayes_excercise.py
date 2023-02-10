89eafebb8e0a57e2b2f484ebee305f7ce4bc71a8
Zen <walao81@gmail.com>
2023-02-10 09:56:27 +0800

    added turtle trading strategy

--- bayes_excercise.py
+++ bayes_excercise.py
@@ -6,6 +6,32 @@ import functools
 from bayes import conditional, prob
 
 
+def turtle_sell_strategy(df, upper_sample=20, lower_sample=10, ATR_sample=20):
+    df = enricment_turtle(df, upper_sample, lower_sample, ATR_sample)
+
+    sell = []
+    time_cost = []
+    for i, _v in enumerate(zip(df.buy.values, df.ATR.values)):
+        buy, buy_atr = _v
+        buy_atr = buy_atr * 2
+
+        sell_point = None
+        days = None
+        for j, v in enumerate(zip(df.Close.values[i+1:], df.turtle_l.values[i+1:], df.ATR.values[i+1:])):
+            _close, _turtle_low, _atr = v
+            sell_point, days = (_close, j) if (_atr > buy_atr) or (_close < _turtle_low) else (None, None)
+            if sell_point:
+                break
+
+        if sell_point:
+            sell.append(sell_point)
+            time_cost.append(max(days, 0.5))
+        else:
+            sell.append(None)
+            time_cost.append(None)
+    return sell, time_cost
+
+
 def grid_sell_strategy(df, stop_loss, take_profit):
     sell = []
     time_cost = []
@@ -41,13 +67,36 @@ def enrichment_daily_profit(df, func_sell_strategy):
     return df
 
 
+def enrichment_flags(df):
+    pass
+
+
+def enricment_turtle(df, upper_sample=20, lower_sample=10, ATR_sample=20):
+    tt_df = pd.DataFrame()
+    tt_df['Date'] = df.Date
+    tt_df['Close'] = df.Close
+    tt_df['turtle_h'] = df.Close.shift(1).rolling(upper_sample).max()
+    tt_df['turtle_l'] = df.Close.shift(1).rolling(lower_sample).min()
+    tt_df['h-l'] = df.High - df.Low
+    tt_df['c-h'] = (df.Close.shift(1)-df.High).abs()
+    tt_df['c-l'] = (df.Close.shift(1)-df.Low).abs()
+    tt_df['TR'] = tt_df[['h-l', 'c-h', 'c-l']].max(axis=1)
+    tt_df['ATR'] = (tt_df.TR.rolling(ATR_sample).sum()/ATR_sample)
+    df['turtle_h'] = tt_df['turtle_h']
+    df['turtle_l'] = tt_df['turtle_l']
+    df['ATR'] = tt_df['ATR']
+    return df
+
 class Strategy(object):
 
 
     def __init__(self, df):
-        func_sell_strategy = functools.partial(grid_sell_strategy, stop_loss=1-loss_margin, take_profit=1+profit_margin)
+        func_sell_strategy = functools.partial(turtle_sell_strategy, upper_sample=30, lower_sample=10, ATR_sample=30)
+        # func_sell_strategy = functools.partial(grid_sell_strategy, stop_loss=1-loss_margin, take_profit=1+profit_margin)
         self.df = enrichment_daily_profit(df, func_sell_strategy)
+        self.df = enrichment_flags(self.df)
         self.profit = df.profit > 0
+        self.tt_buy = (df.Close > df.turtle_h)
 
 
     def dataset(self, name):
@@ -131,7 +180,7 @@ if __name__ == '__main__':
     pd.set_option('display.width', 300)
     df = pd.read_csv('data/BTC-USD.csv')
     df = df.dropna()
-    train, test = split_train_test(df, 0.8)
+    train, test = split_train_test(df, 0.2)
     s = Strategy(train)
     print(s.df)
     print(s.kelly_table(fund=100))
